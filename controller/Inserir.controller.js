5c8a3a02569cff55b44c845fe601f6c4a0d73fd4
Anderson Carlos <anderson.marinho@inventsoftware.com.br>
2021-08-13 18:02:15 -0300

    Primeira versao

---
+++ controller/Inserir.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function (Controller, History) {
+	"use strict";
+
+	return Controller.extend("invent.clientes.controller.Inserir", {
+
+		onInit: function () {
+            this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+        },
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
+		}
+		
+
+		
+
+	});
+});
---
+++ controller/Lista.controller.js
@@ -0,0 +1,44 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function (Controller, History) {
+	"use strict";
+
+	return Controller.extend("invent.clientes.controller.Lista", {
+
+		onInit: function () {
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		},
+
+		inserirCadastroNovo: function() {
+
+			console.log()
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("cadastroDeCliente");
+		},
+
+
+		navegarParaDetalhes: function (oEvent) {
+
+			var oItem = oEvent.getSource();
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("detail", {
+				invoicePath: window.encodeURIComponent(oItem.getBindingContext("invoice").getPath().substr(1))
+			});
+		},
+
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
+		}
+		
+
+	});
+});
