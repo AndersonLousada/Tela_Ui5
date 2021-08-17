e6d25dee987fdec6cdd9ffc7c36f46ac5c14c67c
Anderson Carlos <anderson.marinho@inventsoftware.com.br>
2021-08-17 18:23:26 -0300

    Adicionado contagem de clientes

--- controller/Overview.controller.js
+++ controller/Overview.controller.js
@@ -1,16 +1,35 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
 
-], function (Controller) {
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("invent.clientes.controller.Overview", {
 		
+		onInit: function () {
+
+			 const quantidadeClient = this.funcaoQueTrazQuantidadeDeClientes();
+
+			 let tilesJson = { qtd: quantidadeClient };
+			 let model = new JSONModel(tilesJson)
+
+			this.getView().setModel(model, "tiles");
+        },
+
+		funcaoQueTrazQuantidadeDeClientes : function (){
+
+			let modelInvoice = this.getOwnerComponent().getModel("invoice");
+
+			return modelInvoice.getData().Invoices.length;
+		},
+
 		navegarParaLista : function (){
 			var oRouter = this.getOwnerComponent().getRouter();
 				 oRouter.navTo("listaName");
 				
 		},
+
 		inserirCadastroNovo: function() {
 
 			var oRouter = this.getOwnerComponent().getRouter();
