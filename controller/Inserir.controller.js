d79c7f11d7e768302aa72e086a7c6161863354c6
Anderson Carlos <anderson.marinho@inventsoftware.com.br>
2021-08-18 17:05:43 -0300

    Adicionado pesquisa dinâmica de cep

--- controller/Inserir.controller.js
+++ controller/Inserir.controller.js
@@ -1,16 +1,36 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/routing/History",
-	"sap/m/MessageToast",
-	"sap/m/MessageBox"
-], function (Controller, History, MessageToast, MessageBox ) {
+	"sap/m/MessageBox",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, History, MessageBox, JSONModel) {
 	"use strict";
 
 	return Controller.extend("invent.clientes.controller.Inserir", {
 
 		onInit: function () {
-            this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-        },
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+
+			let cliente = {
+				Cidade: "",
+				Cep: "",
+				Estado: ""
+			};
+
+			this.setClienteModel(cliente);
+
+		},
+
+		setClienteModel: function (data) {
+			var oModel = new JSONModel(data);
+			this.getView().setModel(oModel, "cliente");
+		},
+
+		getClienteModel: function (data) {
+			var oModel = new JSONModel(data);
+			return this.getView().getModel("cliente").getData();
+		},
+
 		onNavBack: function () {
 			var oHistory = History.getInstance();
 			var sPreviousHash = oHistory.getPreviousHash();
@@ -22,16 +42,39 @@ sap.ui.define([
 				oRouter.navTo("overview", {}, true);
 			}
 		},
-		voltarParaInicio: function() {
+		voltarParaInicio: function () {
 
 			var oRouter = this.getOwnerComponent().getRouter();
 			oRouter.navTo("voltarParaInicio");
 		},
-		onConfirmationMessageBoxPress: function () {
-			MessageBox.confirm("Confirme sempre as informações antes de salvar.");
-		}
 
-		
+		buscarEndereco: async function () {
+
+			try {
+
+				let cliente = this.getClienteModel();
+				const url = `https://viacep.com.br/ws/${cliente.Cep}/json/`;
+				const dados = await fetch(url);
+				const endereco = await dados.json();
+
+				cliente.Rua = endereco.logradouro;
+				cliente.Numero = endereco.gia;
+				cliente.Bairro = endereco.bairro;
+				cliente.Cidade = endereco.localidade;
+				cliente.Estado = endereco.uf;
+
+				this.setClienteModel(cliente);
+
+
+			} catch (error) {
+				MessageBox.error(`Erro ao fazer consulta! ${error}`);
+			}
+		},
+
+
+		onConfirmationMessageBoxPress: function () {
+
+		},
 
 		
 
